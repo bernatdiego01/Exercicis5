e5b1c0a8e599ac1a85689bbfaae9a1b2ed686b45
Bernat Diego Carreras <91748429+bernatdiego01@users.noreply.github.com>
2022-11-11 20:55:18 +0100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,41 +1,14 @@
-package com.example.exercici10;
+package com.example.exercici9radio;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.Gravity;
-import android.text.TextWatcher;
-import android.widget.LinearLayout;
-import android.widget.RadioGroup;
-import android.widget.EditText;
-public class MainActivity extends Activity
-        implements RadioGroup.OnCheckedChangeListener {
-    RadioGroup orientation;
-    RadioGroup gravity;
+
+public class MainActivity extends AppCompatActivity {
+
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        orientation=(RadioGroup)findViewById(R.id.orientation);
-        orientation.setOnCheckedChangeListener(this);
-        gravity=(RadioGroup)findViewById(R.id.gravity);
-        gravity.setOnCheckedChangeListener(this);
     }
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-        switch (checkedId) {
-            case R.id.horizontal:
-                orientation.setOrientation(LinearLayout.HORIZONTAL);
-                break;
-            case R.id.vertical:
-                orientation.setOrientation(LinearLayout.VERTICAL);
-                break;
-            case R.id.left:
-                gravity.setGravity(Gravity.LEFT);
-                break;
-            case R.id.center:
-                gravity.setGravity(Gravity.CENTER_HORIZONTAL);
-                break;
-            case R.id.right:
-                gravity.setGravity(Gravity.RIGHT);
-                break;
-        }
-    }}
+}
